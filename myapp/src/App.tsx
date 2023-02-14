6cdb835592afdd30d8d2c744f919b417a23112a9
Jacob Pascua <pascuajakecreed@gmail.com>
2023-02-13 19:24:27 -0500

    boop

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Header } from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
