9b289cd6b06097914e48f8cbd0127745207b75d5
Jacob Pascua <pascuajakecreed@gmail.com>
2023-02-14 12:42:59 -0500

    fixed up some services

--- myapp/src/components/RecipeItem/RecipeItem.tsx
+++ myapp/src/components/RecipeItem/RecipeItem.tsx
@@ -1,68 +1,22 @@
 import * as React from 'react';
 import "./recipeItem.css";
+import { Recipe } from '../../model/RecipeByRandom';
+
+interface IRecipeItemProps{
+  recipe: Recipe
+}
 
 
-export function RecipeItem (){
+export function RecipeItem (props:IRecipeItemProps){
   return (
     <div className="Recipe_List">
 
     <div className="Recipe_Card">
       <div className="Recipe_Image">
-        <img src="https://www.inspiredtaste.net/wp-content/uploads/2016/06/Brownies-Recipe-1-1200.jpg" alt="" />
+        <img src={props.recipe.image} alt="" />
       </div>
       <div className="Recipe_Name">
-        <p>Recipe Name</p>
-      </div>
-      <div className="Recipe_Props">
-        <div className="Prop1"><p>Prop 1</p></div>
-        <div className="Prop2"><p>Prop 2</p></div>
-      </div>
-      <div className="Favorite">
-        <a href="#">Add to Favorites</a>
-        <a href="#"><i className="fa-sharp fa-solid fa-star"></i></a>
-      </div>
-    </div>
-
-    <div className="Recipe_Card">
-      <div className="Recipe_Image">
-        <img src="https://www.inspiredtaste.net/wp-content/uploads/2016/06/Brownies-Recipe-1-1200.jpg" alt="" />
-      </div>
-      <div className="Recipe_Name">
-        <p>Recipe Name</p>
-      </div>
-      <div className="Recipe_Props">
-        <div className="Prop1"><p>Prop 1</p></div>
-        <div className="Prop2"><p>Prop 2</p></div>
-      </div>
-      <div className="Favorite">
-        <a href="#">Add to Favorites</a>
-        <a href="#"><i className="fa-sharp fa-solid fa-star"></i></a>
-      </div>
-    </div>
-
-    <div className="Recipe_Card">
-      <div className="Recipe_Image">
-        <img src="https://www.inspiredtaste.net/wp-content/uploads/2016/06/Brownies-Recipe-1-1200.jpg" alt="" />
-      </div>
-      <div className="Recipe_Name">
-        <p>Recipe Name</p>
-      </div>
-      <div className="Recipe_Props">
-        <div className="Prop1"><p>Prop 1</p></div>
-        <div className="Prop2"><p>Prop 2</p></div>
-      </div>
-      <div className="Favorite">
-        <a href="#">Add to Favorites</a>
-        <a href="#"><i className="fa-sharp fa-solid fa-star"></i></a>
-      </div>
-    </div>
-
-    <div className="Recipe_Card">
-      <div className="Recipe_Image">
-        <img src="https://www.inspiredtaste.net/wp-content/uploads/2016/06/Brownies-Recipe-1-1200.jpg" alt="" />
-      </div>
-      <div className="Recipe_Name">
-        <p>Recipe Name</p>
+        <p>{props.recipe.title}</p>
       </div>
       <div className="Recipe_Props">
         <div className="Prop1"><p>Prop 1</p></div>
