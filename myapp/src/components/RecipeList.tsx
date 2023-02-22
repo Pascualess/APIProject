0749a82103949697e3221fc96029976ed4c72c80
Jacob Pascua <pascuajakecreed@gmail.com>
2023-02-21 22:44:53 -0500

    pagination seems to be working

--- myapp/src/components/RecipeList.tsx
+++ myapp/src/components/RecipeList.tsx
@@ -1,19 +1,54 @@
-import * as React from 'react';
-import { RecipeItem } from './RecipeItem';
-import '../css/recipeList.css'
-import { StandardRecipe } from '../model/StandardRecipe';
+import * as React from "react";
+import { RecipeItem } from "./RecipeItem";
+import "../css/recipeList.css";
+import { StandardRecipe } from "../model/StandardRecipe";
+import _ from "lodash";
+import { useState, useEffect } from "react";
 
 export interface IRecipeListProps {
-  recipes:StandardRecipe[]
+  recipes: StandardRecipe[];
 }
 
-export function RecipeList (props: IRecipeListProps) {
-  
+export function RecipeList(props: IRecipeListProps) {
+
+  const pageSize = 15;
+  const pageCount = Math.ceil(props?.recipes.length / pageSize);
+  const [currentPage, setCurrentPage] = useState(1)
+  const [paginatedPost, setPaginatedPost] = useState<StandardRecipe[]>([]);
+  useEffect(() => {
+    setPaginatedPost(_(props.recipes).slice(0).take(pageSize).value())
+  }, [pageCount]);
+
+  if (pageCount === 1) return null;
+  const pages = _.range(1, pageCount + 1);
+
+  const pagination = (pageNo:number) => {
+    setCurrentPage(pageNo)
+    const startIndex = (pageNo - 1)* pageSize;
+    const paginatedPost = _(props.recipes).slice(startIndex).take(pageSize).value()
+    setPaginatedPost(paginatedPost)
+  }
+
   return (
-    <div className='RecipeList'>
-      <div className='singleRecipe'>
-        {props.recipes.map((recipe) => <RecipeItem recipe={recipe} />)}
+    <div className="RecipeList">
+      <div className="singleRecipe">
+        {paginatedPost.map((recipe) => (
+          <RecipeItem recipe={recipe} />
+        ))}
       </div>
+      <nav className="paginationContainer">
+        <ul className="pagination">
+          {pages.map((page) => (
+            <li className={
+              page === currentPage? "page-item active"  : "page-item"
+            }
+            >
+              <p className="page-link"
+              onClick={() =>pagination(page) }>{page}</p>
+              </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
--- myapp/src/components/SearchBar.tsx
+++ myapp/src/components/SearchBar.tsx
@@ -98,7 +98,7 @@ export function SearchBar(props: ISearchBarProps) {
         )}
         <button id="btn-search" onClick={onSearchClick}>Search</button>
       </div>
-      {recipes && <RecipeList recipes={recipes}/> }
+      {recipes &&<RecipeList recipes={recipes}/>}
     </div>
   );
 }
